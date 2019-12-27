4789b1a5af8c39252c8abe18d1da5fc745bef2cf
Divyansh Thakur <divyansh2140@gmail.com>
2019-12-27 17:04:35 +0530

    Improved the password algo-now supports backspace

--- header/AccountHandler.h
+++ header/AccountHandler.h
@@ -31,7 +31,7 @@ void login() {
               << std::setw(2) << ""
               << " LOGIN "                             // display menu title
               << std::setw(width_menu - 9) << ""
-              << std::endl;
+              << std::endl << std::endl;
 
     for(auto c: username) {
         std::cout << c;
@@ -46,16 +46,23 @@ void login() {
     }
 
     char c;
-    while(c = getch())  {           // taking password from user
-        if(c=='\r') break;
-        pass += c;
-        std::cout << "*";
+    while(c = getch())  {               // taking password from user
+        if(c=='\r') break;              // if user press enter end while loop and save password
+
+        if(c=='\b'&& pass.size()) {     // cheking backspace and limit it to size of password
+            std::cout << "\b \b";       // remove last element from console
+            pass.pop_back();            // remove last element from pass string
+        }
+        else if(c>='!' && c<= '~' && c != '<' && c != '>') {    // checking valid password
+        pass.push_back(c);              // add element at last of pass string
+        std::cout << "*";               // display '*' to console by replacing password
+        }
     }
     std::cout << std::endl
               << std::setw(width_menu) << ""
               << std::setfill(' ') << std::endl;
 
-    load();                         // animate loading screen
+    load();                           // animate loading screen
 
     //mainMenu(username);             // calling the main menu screen to show all program list
 
