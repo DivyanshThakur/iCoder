645ceee5345fe47ce36b3a2263fb0eee16de5782
DivyanshThakur <divyansh2140@gmail.com>
2020-01-20 12:46:31 +0530

    Changed menu order for Home screen

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -23,7 +23,7 @@ const std::string txtString{"String"};
 
 // menuhandler.h
 const std::string main_menu_data{"1. Log In\n2. Create Account\n3. Try (Anonymous)\n4. Users\n5. About\n6. Settings\n7. Exit"};
-const std::string home_data{"1. Array\n2. Recursion\n3. Linked List\n4. Main Menu (Sign Out)\n5. Exit"};
+const std::string home_data{"1. Recursion\n2. Array ~ TO BE IMPLEMENTED\n3. Linked List\n4. Main Menu (Sign Out)\n5. Exit"};
 const std::string settings_data{"1. Change menu speed\n2. Change warning speed\n3. Main Menu\n4. Exit"};
 
 //accounthandler.h
