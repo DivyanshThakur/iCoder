b0abe25b06e0fcba1877fb1ccfb739494b396ca1
DivyanshThakur <divyansh2140@gmail.com>
2020-03-30 11:55:52 +0530

    modified string code

--- namespace/header/cod_string.hpp
+++ namespace/header/cod_string.hpp
@@ -26,21 +26,42 @@ class string
     }
 
     char *str;
-    size_t _size;
-    size_t _max_size;
+    size_t _size, _max_size;
 
 public:
     static constexpr size_t npos = -1;
 
+    /*************************************************** CONSTRUCTOR *****************************************************/
     string();                                                 // no-args
-    string(const char *s);                                    // paramaterized
+    string(const char *s);                                    // from c-string
+    string(const char *s, size_t n);                          // from buffer
+    string(size_t n, char c);                                 // fill
     string(const string &rhs);                                // copy
     string(string &&rhs);                                     // move
     string(const std::initializer_list<char> &list);          // initializer list
     string(const string &rhs, size_t pos, size_t len = npos); // substring
+    // string(const iterator &first, const iterator &last);      // range
 
-    string &operator=(const string &rhs);
-    string &operator=(string &&rhs);
+    /************************************************* ASSIGNMENT OPERATOR ***********************************************/
+    string &operator=(const string &rhs);                       // copy
+    string &operator=(string &&rhs);                            // move
+    string &operator=(const char *s);                           // c-string
+    string &operator=(char c);                                  // character
+    string &operator=(const std::initializer_list<char> &list); // initializer list
+
+    /*************************************************** STRING ITERATOR **************************************************/
+    // iterator begin() const;
+    // iterator end() const;
+    // const const_iterator cbegin() const;
+    // const const_iterator cend() const;
+
+    /****************************************************** CAPACITY *******************************************************/
+    size_t size() const;
+    size_t length() const;
+    // size_t max_size() const;
+    // bool empty() const;
+    // void update_size(int x);
+    // void clear();
 
     ~string();
 };
--- namespace/src/cod_string.cpp
+++ namespace/src/cod_string.cpp
@@ -3,6 +3,8 @@
 #include "../header/cod_limits.hpp"
 #include "../header/cod_algorithm.hpp"
 
+/*************************************************** CONSTRUCTOR *********************************************************/
+
 cod::string::string() : string(nullptr)
 {
 }
@@ -11,7 +13,8 @@ cod::string::string(const char *s) : str(nullptr), _size(0), _max_size(cod::limi
 {
     if (s == nullptr)
     {
-        str = new char[1];
+        _size = 1;
+        str = new char[_size];
         *str = '\0';
     }
     else
@@ -22,17 +25,35 @@ cod::string::string(const char *s) : str(nullptr), _size(0), _max_size(cod::limi
     }
 }
 
+cod::string::string(const char *s, size_t n) : str(nullptr), _size(0), _max_size(cod::limits<size_t>::max())
+{
+    _size = n + 1;
+    str = new char[_size];
+    strncpy(str, s, n);
+}
+
+cod::string::string(size_t n, char c) : str(nullptr), _size(0), _max_size(cod::limits<size_t>::max())
+{
+    _size = n + 1;
+    str = new char[_size];
+
+    for (size_t i{0}; i < n; ++i)
+        str[i] = c;
+
+    str[n] = '\0';
+}
+
 cod::string::string(const string &rhs) : str(nullptr), _size(0), _max_size(cod::limits<size_t>::max())
 {
-    _size = strlen(rhs.str) + 1;
+    _size = rhs._size;
     str = new char[_size];
     strcpy(str, rhs.str);
 }
 
 cod::string::string(string &&rhs) : str(nullptr), _size(0), _max_size(cod::limits<size_t>::max())
 {
-    str = rhs.str;
     _size = rhs._size;
+    str = rhs.str;
     rhs.str = nullptr;
 }
 
@@ -67,25 +88,87 @@ cod::string::string(const string &rhs, size_t pos, size_t len) : str(nullptr), _
     str[i] = '\0';
 }
 
+/************************************************* ASSIGNMENT OPERATOR ***************************************************/
+
 cod::string &cod::string::operator=(const string &rhs)
 {
-    delete[] str;
+    // delete[] str;
 
-    str = new char[strlen(rhs.str) + 1];
-    strcpy(str, rhs.str);
+    // _size = rhs._size;
+
+    // str = new char[_size];
+    // strcpy(str, rhs.str);
 
     return *this;
 }
 
 cod::string &cod::string::operator=(string &&rhs)
 {
-    strcpy(str, rhs.str);
-    rhs.str = nullptr;
+    // delete[] str;
+    // _size = rhs._size;
+
+    // str = rhs.str;
+    // rhs.str = nullptr;
 
     return *this;
+}
+
+cod::string &cod::string::operator=(const char *s)
+{
+    // delete[] str;
+    // _size = strlen(s) + 1;
+    // str = new char[_size];
+
+    // strcpy(str, s);
+
+    return *this;
+}
+
+cod::string &cod::string::operator=(char c)
+{
+    // delete[] str;
+
+    // _size = 2;
+    // str = new char[_size];
+    // str[0] = c;
+    // str[1] = '\0';
+
+    return *this;
+}
+
+cod::string &cod::string::operator=(const std::initializer_list<char> &list)
+{
+    // delete[] str;
+
+    // _size = list.size() + 1;
+    // str = new char[_size];
+
+    // size_t i{0};
+
+    // for (const auto &c : list)
+    //     str[i++] = c;
+
+    // str[i] = '\0';
+
+    return *this;
+}
+
+/*************************************************** STRING ITERATOR *****************************************************/
+
+/****************************************************** CAPACITY *********************************************************/
+
+size_t cod::string::size() const
+{
+    return strlen(str);
+}
+
+size_t cod::string::length() const
+{
+    return strlen(str);
 }
 
 cod::string::~string()
 {
     delete[] str;
+    _size = 0;
 }
